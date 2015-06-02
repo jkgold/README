ad0c4313e3e0b0f73d47f1760b0e7f48f2f27a0d
jkgold <Jasonkarlgoldfarb@gmail.com>
2015-06-02 14:21:30 -0600

    checkpoint 36 completeduser authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
 
 
+  devise_for :users
+  get 'questions/index'
+
+  get 'questions/new'
+
+  get 'questions/create'
+
+  get 'questions/show'
+
+  get 'questions/edit'
+
+  get 'questions/update'
+
+  get 'questions/destroy'
+
 resources :posts, :advertisements
 
 
