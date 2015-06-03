141ef37f5c3e6d186d727c552f7273984cd10cab
jkgold <Jasonkarlgoldfarb@gmail.com>
2015-06-03 12:28:11 -0600

    Questions kind of working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,24 @@
 class QuestionsController < ApplicationController
-	def index
-		@question = Question.all
-	end
+  def index
+    @questions = Question.all
+  end
 
+  def new
+    @question = Question.new
+  end
 
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
