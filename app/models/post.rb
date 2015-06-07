f7c586745da335d3615cffb2eac05845c575c6ca
jkgold <Jasonkarlgoldfarb@gmail.com>
2015-06-07 12:55:40 -0600

    checkpoint 20 Address bloc completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,12 @@ class Post < ActiveRecord::Base
 
 
 	default_scope { order('created_at DESC')}	
-end
+	scope :ordered_by_title => Post.sort_by { |p| p.class == Array? p.first :p}
+	scope :ordered_by_reversere_created_at => Post.sort! |first, last| last <=> first end
+ 		end
+ 	
+
+	
+
+
+# a.sort_by { |f| f.class == Array ? f.first : f }
